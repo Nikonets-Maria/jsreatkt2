e2a5b17a6250f006de90bf0754c8c676666a5cb8
Мария Никонец <137428433+Nikonets-Maria@users.noreply.github.com>
2024-10-02 16:41:29 +0300

    Update AddForm.jsx

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,17 +1,56 @@
 
-function AddForm(props){
-    const {createProduct} = props
-    function handletKeyUp(event){
-        if (event.key === 'Enter'){
-            createProduct(event.target.value)
-            
+// function AddForm(props){
+//     const {createProduct} = props
+//     function handletKeyUp(event){
+//         if (event.key === 'Enter'){
+//             createProduct(event.target.value, event.target.value)
+//         }
+//     }
+// return(
+//     <div className='input_elem'>
+//         <input onKeyUp={handletKeyUp}/>
+//         {/* <input onKeyUp={handletKeyUp}/> */}
+//     </div>)
+// }
+
+// export default AddForm
+
+
+
+//костыль переписать 
+import React, { useState } from 'react';
+
+function AddForm(props) {
+    const { createProduct } = props;
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+
+    function handleKeyUp(event) {
+        if (event.key === 'Enter') {
+            createProduct(name, price);
+            setName(''); // Сброс значения имени
+            setPrice(''); // Сброс значения цены
         }
     }
-return(
-    <div className='input_elem'>
-        <input onKeyUp={handletKeyUp}/>
-        
-    </div>)
+
+    return (
+        <div className='input_elem'>
+            <input 
+                type="text" 
+                placeholder="Введите имя продукта" 
+                value={name}
+                onChange={(e) => setName(e.target.value)} 
+                onKeyUp={handleKeyUp}
+            />
+            <input 
+                type="text" 
+                placeholder="Введите цену продукта" 
+                value={price}
+                onChange={(e) => setPrice(e.target.value)} 
+                onKeyUp={handleKeyUp}
+            />
+        </div>
+    );
 }
 
-export default AddForm
+export default AddForm;
